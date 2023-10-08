6500766f8e3fbc8a8ec4f43ad0b381f79bbab523
sai2963 <venkatsai0629@gmail.com>
2023-10-08 04:33:25 -0700

    file update

---
+++ app.js
@@ -0,0 +1,5 @@
+//let anchorelement=document.getElementById('exlink');
+//anchorelement.href='https://web.whatsapp.com/'
+
+let anchorelements=document.querySelector('a');
+anchorelements.href='https://google.com/'
---
+++ exercise.js
@@ -0,0 +1,10 @@
+console.log(document.body.children)
+let a= document.body.children[0];
+console.log(a.parentElement)
+console.log(a.nextElementSibling)
+let b=document.getElementById('hey')
+console.log(b)
+let c=document.querySelector('a')
+console.log(c)
+c.textContent='changed by sai'
+c.href='https://github.com/sai2963/duduka_advancedjavascript'
